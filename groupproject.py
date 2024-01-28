f92634d87beb25a80bdf25cc1e4d6b97ab035568
conorpwalsh <cpw58@cornell.edu>
2024-01-28 14:53:20 -0500

    fixed some stuff once I moved out of collab and into local

--- groupproject.py
+++ groupproject.py
@@ -24,38 +24,39 @@ random.seed(540)
 from config.definitions import ROOT_DIR
 path_to_data = os.path.join(ROOT_DIR, 'data', 'OnlineNewsPopularity.csv')
 
-df = pd.read_csv(path_to_data)
 all_data = pd.read_csv(path_to_data)
-df['id'] = df.index + 1 
+all_data['id'] = all_data.index + 1 
 
-first_column = df.pop('id')
-df.insert(0,'id', first_column)
+first_column = all_data.pop('id')
+all_data.insert(0,'id', first_column)
 
-print(df.head(5))
+print(all_data.head(5))
 
-
+##
 #EDA and Visualizations
+all_data.info()
+
+all_data.describe()
 all_data['Day_of_Week'] = ''
 warnings.filterwarnings('ignore')
 
 weekdays = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 
-all_data.columns = all_data.columns.str.replace(' ', '') #remove white space
+all_data.columns = all_data.columns.str.replace(' ', '') #remove white space from all columns
 weekday_cols = all_data.filter(like='weekday_is_').columns.tolist() #Days of the week columns
 
 wday_dict = dict(zip(weekday_cols,weekdays))
-%pdb on
+# Loop through each row and find where the day of week column is flagged, use weekday dict to populate new factor column
 for i in range(all_data.shape[0]):
   for key, value in wday_dict.items():
     if all_data[str(key)][i] == 1:
       day = value
-      #print(i,day)
       break
   all_data['Day_of_Week'][i] = day
 
-print(all_data['Day_of_Week'].unique())
-new_all_data = all_data.drop(weekday_cols, axis=1)
-new_all_data.shape
+print(all_data['Day_of_Week'].unique()) #Make sure this works!
+new_all_data = all_data.drop(weekday_cols, axis=1) #Don't need these anymore
+new_all_data.shape #Verify change in num cols
 
 f, axs = plt.subplots(1, 2, figsize=(24, 8))
 sns.kdeplot(data=new_all_data, x = new_all_data['shares']/10000, hue = 'Day_of_Week', multiple = 'stack', ax = axs[0], palette="husl")
@@ -63,17 +64,12 @@ plt.xlabel('Shares (in ten thousands)')
 sns.kdeplot(data=new_all_data, x = np.log(new_all_data['shares']+1), hue = 'Day_of_Week', multiple = 'stack', ax = axs[1], palette="husl")
 plt.xlabel('Shares')
 plt.ylabel('Density')
+plt.show()
 
 
 
-column_list = df.columns.tolist()
-print(column_list)
-
-
-df.info()
-
-df.describe()
-
+#column_list = df.columns.tolist()
+#print(column_list)
 
 
 #df.corr()
